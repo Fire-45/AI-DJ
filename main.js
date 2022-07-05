7fe7df67d8db6b7ab24bfe2c9c3f6884cf51ef4b
Fire-45 <77906966+Fire-45@users.noreply.github.com>
2022-07-05 11:29:48 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var Song1 = "";
 var Song2 = "";
 
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 function preload(){
 
     Song1 = loadSong("music.mp3");
@@ -13,8 +18,31 @@ function setup(){
     canvas.position(370,300);
     video = createCapture(VIDEO);
     video.hide();
+
+    posenet = ml5.poseNet(video,modelLoaded);
+    posenet.on("pose",gotPoses);
+}
+
+function modelLoaded(){
+    
+    console.log("model loaded");
+}
+
+function gotPoses(result){
+
+    console.log(result);
+
+    leftWristX = result[0].pose.leftWrist.x;
+    leftWristY = result[0].pose.leftWrist.y;
+    rightWristX = result[0].pose.rightWrist.x;
+    rightWristY = result[0].pose.rightWrist.y;
+
 }
 
 function draw(){
     image(video,0,0,600,350);
+
+    fill("red");
+    circle(leftWristX,leftWristY,40);
+    circle(rightWristX,rightWristY,40);
 }
